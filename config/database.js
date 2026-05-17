dfd45b99c43a978cebda21b8aa4736fc13cd18b4
09kris <krisheruwala000@gmail.com>
2026-05-17 10:47:16 +0530

    solve db error

--- config/database.js
+++ config/database.js
@@ -3,6 +3,8 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 
+console.log('Connecting to database:', process.env.DATABASE_URL ? process.env.DATABASE_URL.replace(/:[^:@]+@/, ':****@') : 'undefined');
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
   protocol: "postgres",
